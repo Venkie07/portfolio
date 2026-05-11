80cea62fb28979d17e5ce4260ac2fdd74c5199aa
Venkie07 <venkateswaran01092007@gmail.com>
2026-05-11 22:13:34 +0530

    Portfolio : Sitemap.xml and robots.txt created

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import  sitemap  from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({hostname: 'https://venkateswaran-k.web.app'}),
   ],
 })
